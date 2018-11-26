883594e698c65885bee292f75b8f67cb28f717a0
Erick Hideki Nishimoto <ti-erick@ftd.com.br>
2018-11-26 15:39:12 -0200

    Update Geral

--- conecta.php
+++ conecta.php
@@ -1,6 +1,6 @@
 <?php
 
-@$conexao = mysqli_connect('localhost', 'root', 'root', 'casamento_bd') or die(
+@$conexao = mysqli_connect('mysql995.umbler.com', 'jades2allan', 'Casamento**2019', 'casamento_bd') or die(
     header("Location: erro?mysql=".mysqli_connect_errno()));
 mysqli_set_charset($conexao,"utf8");
 mysqli_query($conexao, "SET lc_time_names = 'pt_BR';"); 
