87017e8011f841b6146d64ce2bb8abcafbd18ff2
Erick Nishimoto <erick.nishimoto@gmail.com>
2019-02-10 19:43:26 -0200

    FIX brand link

--- lista-presentes.php
+++ lista-presentes.php
@@ -47,7 +47,7 @@ https://github.com/BlackrockDigital/startbootstrap-agency/blob/gh-pages/LICENSE
     <!-- Navigation -->
     <nav class="navbar navbar-expand-lg navbar-dark fixed-top" id="mainNav">
       <div class="container">
-        <a class="navbar-brand js-scroll-trigger" href="#page-top"><?= ($infos['brand']) ?></a>
+        <a class="navbar-brand js-scroll-trigger" href="/"><?= ($infos['brand']) ?></a>
         <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
           Menu
           <i class="fas fa-bars"></i>
