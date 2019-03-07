0722eeda76c9abeaa1da31c308a5cc8879da0eed
Erick Nishimoto <erick.nishimoto@gmail.com>
2019-03-06 21:33:09 -0300

    Update 06/03

--- index.php
+++ index.php
@@ -551,30 +551,30 @@ https://github.com/BlackrockDigital/startbootstrap-agency/blob/gh-pages/LICENSE
     let bg = document.querySelector(".masthead");
     bg.style.background= "linear-gradient(0deg,rgba(0, 0, 0, 0),rgba(0, 0, 0, .4)),url('upload/<?= $infos['cabecalho_imagem']?>')";
     bg.style.backgroundRepeat = "no-repeat";
-    bg
+    bg.style.backgroundAttachment = "fixed";
     bg.style.backgroundPosition = "center";
     bg.style.backgroundSize = "cover";
 
     let bg2 = document.querySelector("#mensagens");
     bg2.style.background= "linear-gradient(0deg,rgba(0, 0, 0, 0),rgba(0, 0, 0, .2)),url('upload/<?= $infos['mensagens_imagem']?>";
     bg2.style.backgroundRepeat = "no-repeat";
-    bg2
+    bg2.style.backgroundAttachment = "fixed";
     bg2.style.backgroundPosition = "center";
-    bg2.style.backgroundSize = "cover";
+    bg2.style.backgroundSize = "scale-down";
 
     let bg3 = document.querySelector("#local");
     bg3.style.background= "linear-gradient(0deg,rgba(0, 0, 0, 0),rgba(0, 0, 0, .8)),url('img/bg-3.jpg')";
     bg3.style.backgroundRepeat = "no-repeat";
-    bg3
+    bg3.style.backgroundAttachment = "fixed";
     bg3.style.backgroundPosition = "center";
-    bg3.style.backgroundSize = "cover";
+    bg3.style.backgroundSize = "scale-down";
 
     let bg4 = document.querySelector("#fotos");
     bg4.style.background= "linear-gradient(0deg,rgba(0, 0, 0, 0.1),rgba(0, 0, 0, 0.4)),url('img/bg-4.jpg')";
     bg4.style.backgroundRepeat = "no-repeat";
-    bg4
+    bg4.style.backgroundAttachment = "fixed";
     bg4.style.backgroundPosition = "center";
-    bg4.style.backgroundSize = "cover";
+    bg4.style.backgroundSize = "scale-down";
     </script>
 
     <!-- Limitador de caracteres text-area -->
