3bf80a1d21f0cc76140b28b82a5d6773b47fc017
Erick Hideki Nishimoto <ti-erick@ftd.com.br>
2019-03-08 10:44:35 -0300

    Add button category to gift

--- lista-presentes.php
+++ lista-presentes.php
@@ -112,11 +112,12 @@ https://github.com/BlackrockDigital/startbootstrap-agency/blob/gh-pages/LICENSE
 
                  <div class="col-lg-3 col-md-4 mt-4">
                   <a target="_blank" href="<?= ($presente->link) ?>" class="link">
-                    <div class="card-presentes d-flex justify-content-center">
+                    <div class="card-presentes d-flex justify-content-center text-center">
                       <img class="card-img-top img-md3" src="upload/<?= $presente->imagem ?>" alt="Card image cap">
                       <p class="card__titulo d-flex card__titulo justify-content-center align-items-center text-center"><?= $presente->titulo ?></p>                    
                       <p class="card__autor text-center">Valor médio:</p>
                       <p class="card__preco text-center">R$ <?= $presente->valor ?></p>
+                      <span class="badge badge-presente mb-2"><?= $presente->categoria ?></span>
                       <a target="_blank" href="<?= $presente->link ?>" class="btn botao-comprar">Comprar</a>
                     </div>
                   </a>
