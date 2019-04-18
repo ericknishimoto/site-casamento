0fb31a6280bb82c5c8fe50d6235ac30c1ec708df
Erick Hideki Nishimoto <ti-erick@ftd.com.br>
2019-04-18 05:57:18 -0300

    Test .htaccess error 500

---
+++ error.php
@@ -0,0 +1,251 @@
+<?php
+require_once 'conecta.php';
+require_once 'banco-meusite.php';
+
+$infos = listaMeusite($conexao);
+$presentes = listaPresentes($conexao);
+$categorias = listaCategorias($conexao);
+?>
+
+<html lang="en">
+
+  <head>
+
+<!--
+Arquitetura de layout por Blackrock Digital LLC.
+Veja mais em:
+https://github.com/BlackrockDigital/startbootstrap-agency/blob/gh-pages/LICENSE
+-->
+    
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title><?= ($infos['titulo']) ?> (<?= date('d/m/Y', strtotime($infos['data_casamento'])) ?>)</title>
+    <link rel="shortcut icon" type="image/png" href="dist/img/favicon.ico"/>
+
+    <!-- Bootstrap core CSS -->
+    <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom fonts for this template -->
+    <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+    <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet" type="text/css">
+    <link href='https://fonts.googleapis.com/css?family=Kaushan+Script' rel='stylesheet' type='text/css'>
+    <link href='https://fonts.googleapis.com/css?family=Droid+Serif:400,700,400italic,700italic' rel='stylesheet' type='text/css'>
+    <link href='https://fonts.googleapis.com/css?family=Roboto+Slab:400,100,300,700' rel='stylesheet' type='text/css'>
+
+    <!-- Custom styles for this template -->
+
+    <link href="css/lista-presentes.css" rel="stylesheet">
+    <link href="css/tema-presentes.css" rel="stylesheet">
+    <link href="css/animate.css" rel="stylesheet">
+
+  </head>
+
+  <body id="page-top">
+
+    <!-- Navigation -->
+    <nav class="navbar navbar-expand-lg navbar-dark fixed-top" id="mainNav">
+      <div class="container" style="max-width: 1300px;">
+        <a class="navbar-brand js-scroll-trigger" href="/"><?= ($infos['brand']) ?></a>
+        <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+          Menu
+          <i class="fas fa-bars"></i>
+        </button>
+        <div class="collapse navbar-collapse animated fadeIn" id="navbarResponsive">
+          <ul class="navbar-nav text-uppercase ml-auto"> 
+          <li class="nav-item">
+              <a class="nav-link js-scroll-trigger" href="/">Início</a>
+            </li>   
+          <li class="nav-item">
+              <a class="nav-link js-scroll-trigger" href="/#section01">Sobre</a>
+            </li> 
+            <li class="nav-item">
+              <a class="nav-link js-scroll-trigger" href="/#mensagens">Mensagens</a>
+            </li>      
+            <li class="nav-item">
+                <a class="nav-link js-scroll-trigger" href="/#presenca">Presença</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link js-scroll-trigger" href="/#local">Local</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+
+    <!-- Header -->
+     <section id="presenca" class="presenca text-center">
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-12 text-center">
+            <h2 class="section-heading">Lista de Presentes</h2>
+            <h3 class="section-subheading text-muted">Sem presentes, sem festa =)</h3>
+          </div>
+        </div>
+        <div class="d-flex">
+            <hr class="my-auto flex-grow-1" style="color:gray;">
+            <div class="px-4" style="color:gray;">CATEGORIAS</div>
+            <hr class="my-auto flex-grow-1" style="color:gray;">
+        </div>              
+        
+        <div class="row d-flex justify-content-center">
+        <button type="button" id="categoria" class="btn botao-categoria text-muted mostrarTodos">
+          TODOS            
+        </button>    
+          <?php foreach($categorias as $categoria) :?>
+            <button type="button" id="categoria" class="btn botao-categoria text-muted">
+            <?=$categoria['nome']?>
+            </button>    
+          <?php endforeach ?>  
+        </div>
+
+      </div>
+      <!-- <a class="seta-section js-scroll-trigger" href="#mensagens"><i class="fas fa-angle-down animated pulse infinite"></i></a> -->
+    </section>
+
+   
+
+<!-- Footer -->
+<footer>
+  <div class="container">
+    <div class="row">
+      <div class="col-md-12">
+        <span class="copyright"><?= ($infos['titulo']) ?> - Todos os direitos reservados | Criado por <a href="http://www.ericknishimoto.com.br" target="_blank">Érick Nishimoto</a></span>
+      </div>
+    </div>
+  </div>
+</footer>
+
+<!-- ********************************** MODAL TRANSFERENCIA ********************************** -->
+<div class="modal fade" id="modal-transferencia" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-12">
+              <div class="modal-body">
+              <form action="adiciona-transferencia.php" id="form-transferencia" method="POST">
+                  <div class="row">
+                    <div class="col-xs-12 p-4">
+
+                      <div class="row">
+                        <div class="col-12">
+
+                          <div class="form-group mt-1 text-center">
+                              <label class="titulo-modal">Transferir Valor</label>
+                          </div>
+
+                          <div class="row text-center">
+                            <p class="col-md-12 text-muted">Transfira um valor diretamente para os noivos.</p>
+                          </div>
+
+                          <div class="form-group row text-center">
+                            <div class="col-md-12 text-center mt-1 mb-1">
+                              <span class="h3 text-muted"><b>Itaú</b></span><br>
+                              <span class="h4 text-muted">Agência: 0123</span><br>
+                              <span class="h4 text-muted">C/C: 81234-5</span>
+                            </div>
+                            <!-- <div class="col-md-6 text-center">
+                              <img src="img/pig.png" style="max-height: 100px;">
+                            </div> -->
+                          </div>
+
+                          <div class="row text-center">
+                            <p class="col-md-12 text-muted"><b>*Após fazer a transferência, envie-nos os dados abaixo.</b></p>
+                          </div>
+                        </div>
+
+                        <!-- DIVISÓRIA -->
+                        <div class="col-12 mt-1 mb-1">
+                          <div class="d-flex mb-1">
+                            <hr class="my-auto flex-grow-1" style="color:gray;">
+                            <div class="px-4" style="color:gray;">♥</div>
+                            <hr class="my-auto flex-grow-1" style="color:gray;">
+                          </div>
+                        </div>
+
+                        <div class="col-12">
+                          <div class="form-group mt-1">
+                            <label>Nome</label><span class="text-muted"> (*)</span>
+                            <input type="text" name="nome" class="form-control">
+                            <div class="text-right">
+                            </div>
+                          </div>
+                        </div>
+
+                        <div class="col-6">
+                          <div class="form-group mt-1">
+                            <label>Valor</label><span class="text-muted"> (*)</span>
+                            <input name="valor" class="form-control money">
+                              <div class="text-right">
+                              </div>
+                          </div>
+                        </div>
+
+                        <div class="col-6">
+                          <div class="form-group mt-1">
+                            <label>Data</label><span class="text-muted"> (*)</span>
+                            <input type="date" name="data" class="form-control">
+                              <div class="text-right">
+                              </div>
+                          </div>
+                        </div>
+
+                        <div class="col-12">
+                          <div class="form-group mt-1">
+                            <label>Número de Operação</label><span class="text-muted"> (Opcional)</span>
+                            <input type="text" name="obs" class="form-control">
+                            <div class="text-right">
+                            </div>
+                          </div>
+                        </div>
+
+                      </div>
+                    </div>
+                    <div class="col-12 text-center">
+                        <button class="btn text-muted bg-color-gray mr-2" type="submit" form="form-transferencia" value="Submit">Enviar</button>
+                        <button type="button" class="btn text-muted bg-color-gray" data-dismiss="modal">Fechar</button>
+                    </div>
+                  </div>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+
+    <!-- Bootstrap core JavaScript -->
+    <script src="vendor/jquery/jquery.min.js"></script>
+    <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+    <!-- Plugin JavaScript -->
+    <script src="vendor/jquery-easing/jquery.easing.min.js"></script>
+
+    <!-- Contact form JavaScript -->
+    <script src="js/jqBootstrapValidation.js"></script>
+    <script src="js/contact_me.js"></script>
+
+    <!-- Custom scripts for this template -->
+    <script src="js/agency.min.js"></script>
+
+    <!-- Limitador de caracteres text-area -->
+    <script>
+      $(document).on("input", "#TxtObservacoes", function () {
+        var limite = 200;
+        var caracteresDigitados = $(this).val().length;
+        var caracteresRestantes = limite - caracteresDigitados;
+
+      $(".caracteres").text(caracteresRestantes);
+      });
+    </script>
+
+<script src="js/adicionaFiltro.js"></script>
+<script src="js/filtraPresentes.js"></script>
+
+  </body>
+
+</html>
